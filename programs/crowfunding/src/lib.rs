1cbf03d3899b4e00d49ab6cc07a52db4380c6a64
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-01-29 11:09:10 +0400

    006

--- programs/crowfunding/src/lib.rs
+++ programs/crowfunding/src/lib.rs
@@ -6,11 +6,42 @@ declare_id!("99oEGjcb7XLjSApGPLKbR7XczUhuAs7NUJW7qCSLC9w8");
 pub mod crowfunding {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.account_donated = 0;
+        campaign.admin = *ctx.accounts.user.key;
         Ok(())
     }
+
+  
+    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //     msg!("Greetings from: {:?}", ctx.program_id);
+    //     Ok(())
+    // }
 }
 
+// #[derive(Accounts)]
+// pub struct Initialize {}
+
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space=9000, seeds=[b"CAMPAIGN_DEMO".as_ref(), user.key().as_ref()], bump)]
+    pub campaign: Account<'info, Campaign>,
+    
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
+
+#[account]
+pub struct Campaign {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub account_donated: u64
+}
